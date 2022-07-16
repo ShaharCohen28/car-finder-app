bdbe594dc468cd490ddc35869fce4a3e18898113
shahar <jkunuy27@gmail.com>
2022-07-16 17:25:28 +0300

    first commit

---
+++ src/main/java/com/shahar/carfinderapp/CarFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.shahar.carfinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFinderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahar/carfinderapp/CarFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahar.carfinderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
